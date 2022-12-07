672af935a0c3c1a07e435e735541e9cd50a4aa1c
BakarZ <mimbala.a@gmail.com>
2022-12-07 16:22:18 +0800

    React Router Outlet Applied

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 
+const Shop = () => {
+  return <h1>test</h1>;
+};
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
