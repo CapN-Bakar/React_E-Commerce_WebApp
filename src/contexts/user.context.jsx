e95e7cb794322e81f61cdaa056008165c6823bd5
CapN-Bakar <mimbala.a@gmail.com>
2023-01-28 19:04:45 +0800

    Observer Pattern

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,9 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from "../utilities/firebase/firebase.utils";
 
 //Context provides a way to pass data through the component tree without having to pass props down manually at every level.
 
@@ -12,5 +17,15 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
+    return unsubscribe;
+  }, []);
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
